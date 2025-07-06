e8a5099d3a52884a86498e73616c44f2796c1e64
mrakibur179 <mrakibur179@gmail.com>
2025-07-06 08:43:13 +0600

    added create a post api route

---
+++ backend/models/post.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    poster: {
+      type: String,
+      default:
+        "https://media.sproutsocial.com/uploads/2022/05/How-to-post-on-instagram-from-pc.jpg",
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
