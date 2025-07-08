181f93372dd54fcab788412ed6a6e59af680877b
mrakibur179 <mrakibur179@gmail.com>
2025-07-08 13:24:00 +0600

    added delete users from admin dashboard

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -69,6 +69,25 @@ export const DashUsers = () => {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+        setOpenModal(false);
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="table-auto md:mx-auto scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-600 dark:scrollbar-thumb-slate-800">
       {isLoading ? (
@@ -176,12 +195,12 @@ export const DashUsers = () => {
           <div className="text-center">
             <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this post?
+              Are you sure you want to delete this user?
             </h3>
             <div className="flex justify-center gap-4">
               <Button
                 color="red"
-                // onClick={handleDeletePost}
+                onClick={handleDeleteUser}
                 className="cursor-pointer"
               >
                 Yes, I'm sure
